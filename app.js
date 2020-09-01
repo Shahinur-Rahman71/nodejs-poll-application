07a22ba2357ff0ac4895f457907dd81a67b66c19
Shahinur-Rahman71 <shanto16026@gmail.com>
2020-09-01 21:13:37 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose'); // aita diya database er sathe communication kora jai
+const allController = require('./controller/pollController');
+const port = process.env.port || 4050
+const app = express();//aitar kaj muloto : route handle kora,middleware function niya kaj kora
+// and templete engine niya kaj kora
+
+app.set('view engine', 'ejs');
+// app.set('views', 'templates');// views folder and templates directory;
+// by default express application a sobkisu thake views directory te;
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+
+app.get('/create', allController.createPollGetController)
+app.post('/create', allController.createPollPostController)
+app.get('/polls', allController.getallPolls)
+app.get('/polls/:id', allController.viewPollGetController)
+app.post('/polls/:id', allController.viewPollPostController)
+
+app.get('/',(req, res) => {
+    res.render('home')
+})
+
+mongoose.connect('mongodb://localhost:27017/test')
+    .then( () => {
+        app.listen(port,()=> {
+            console.log(`Application is ready to serve on port ${port}`)
+        }) 
+    })
+    .catch(err =>{
+        console.log(err)
+    })
+
---
+++ controller/pollController.js
@@ -0,0 +1,91 @@
+const Poll = require('../schema/poll');
+
+exports.createPollGetController = (req, res, next) => {
+    res.render('create');
+}
+
+exports.createPollPostController = async (req, res, next) => {
+    let { title, description, options} = req.body;
+
+    options = options.map(option => {
+        return {
+            name: option,
+            vote: 0
+        }
+    })
+
+    //here we create document
+    let poll = new Poll({
+        title,
+        description,
+        options
+    })
+
+    try {
+        await poll.save()
+        res.redirect('/polls')
+
+    } catch (e) {
+        console.log(e)
+    }
+
+    console.log(options)
+
+    res.render('create');
+}
+
+exports.getallPolls = async (req, res, next) => {
+    try{
+        let polls = await Poll.find()
+        res.render('polls', {polls})
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+exports.viewPollGetController = async (req, res, next) => {
+    let id = req.params.id;
+
+    try {
+        let poll = await Poll.findById(id)
+        let options = [...poll.options]
+
+        let result = []
+        options.forEach(option => {
+            let percentage = (option.vote * 100) / poll.totalvote
+            result.push({
+                ...option._doc,
+                percentage: percentage ? percentage: 0
+            })
+        })
+
+        res.render('viewPoll', {poll, result})
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+exports.viewPollPostController = async (req, res, next) => {
+    let id = req.params.id
+    let optionId = req.body.option
+    try {
+        let poll = await Poll.findById(id)
+        let options = [...poll.options]
+        
+        let index = options.findIndex(oo => oo.id === optionId)
+        options[index].vote = options[index].vote + 1
+
+        let totalvote = poll.totalvote + 1
+
+        await Poll.findOneAndUpdate(
+            {_id: poll._id},
+            {$set: {options, totalvote}}
+        )
+
+        res.redirect('/polls/' + id)
+
+    } catch (e) {
+        console.log(e)
+    }
+}
