4ef5359968c48f9bc783c6a5d3a891912e4481e6
Shahinur-Rahman71 <shanto16026@gmail.com>
2021-05-25 22:44:27 +0600

    make it better

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose'); // aita diya database er sathe communication kora jai
 const allController = require('./controller/pollController');
-const port = process.env.PORT || 4050
 const app = express();//aitar kaj muloto : route handle kora,middleware function niya kaj kora
 // and templete engine niya kaj kora
 
@@ -20,20 +20,29 @@ app.get('/polls', allController.getallPolls)
 app.get('/polls/:id', allController.viewPollGetController)
 app.post('/polls/:id', allController.viewPollPostController)
 
-app.get('/',(req, res) => {
-    res.render('create')
-})
 
 //Configure isProduction variable
 const isProduction = process.env.NODE_ENV === 'production';
 
-mongoose.connect(process.env.MONGODB_URL ||'mongodb://localhost:27017/test')
-    .then( () => {
-        app.listen(port,()=> {
-            console.log(`Application is ready to serve on port ${port}`)
-        }) 
-    })
-    .catch(err =>{
-        console.log(err)
-    })
+// connect to database
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if(err) throw err;
+    console.log('Connected to MongoDB');
+});
+
+app.get('/',(req, res) => {
+    res.render('create')
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
 
